91b64749ad4f0722aa0be18ae7d7b844a4a9a60e
roellongcop <longcoproel@gmail.com>
2023-10-22 13:31:35 +0800

    c

--- widgets/views/file-explorer/_folder.php
+++ widgets/views/file-explorer/_folder.php
@@ -1,23 +1,35 @@
 <?php
 
 use app\helpers\Html;
-?>
-<div class="col-md-2">
-	<div class="folder-container mb-2" data-path="<?= $folder['path'] ?>" data-toggle="tooltip" title="Double click to open" data-placement="top">
-	    <img src="<?= $folderImage ?>" class="img-fluid img-create-folder">
-	    <div class="font-weight-bold folder-label"><?= $folderName ?></div>
+use app\helpers\App;
+use app\models\User;
 
-	    <?= Html::ifElse($folder['total_documents'] == 0, 
-	    	function() {
-		    	return Html::tag('label', 0, [
-		    		'class' => 'file-count badge badge-danger'
-		    	]);
-		    },
-	    	function() use($folder) {
-	    		return Html::tag('label', number_format($folder['total_documents']), [
-		    		'class' => 'file-count badge badge-success'
-		    	]);
-		    },
-		) ?>
+$show = true;
+if ($folderName == $folder['path']) {
+	if (App::identity('isAdmin') && User::findOne(['email' => $folderName, 'accountant_id' => App::identity('id')]) === null) {
+		$show = false;
+	}
+}
+?>
+
+<?php if ($show): ?>
+	<div class="col-md-2">
+		<div class="folder-container mb-2" data-path="<?= $folder['path'] ?>" data-toggle="tooltip" title="Double click to open" data-placement="top">
+		    <img src="<?= $folderImage ?>" class="img-fluid img-create-folder">
+		    <div class="font-weight-bold folder-label"><?= $folderName ?></div>
+
+		    <?= Html::ifElse($folder['total_documents'] == 0, 
+		    	function() {
+			    	return Html::tag('label', 0, [
+			    		'class' => 'file-count badge badge-danger'
+			    	]);
+			    },
+		    	function() use($folder) {
+		    		return Html::tag('label', number_format($folder['total_documents']), [
+			    		'class' => 'file-count badge badge-success'
+			    	]);
+			    },
+			) ?>
+		</div>
 	</div>
-</div>
+<?php endif ?>
