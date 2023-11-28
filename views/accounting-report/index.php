69e48e9f48bbb7fe900acd29c5387a37227210da
roellongcop <longcoproel@gmail.com>
2023-11-28 19:05:18 +0800

    Update index.php

--- views/accounting-report/index.php
+++ views/accounting-report/index.php
@@ -17,15 +17,34 @@ $this->registerCss(<<< CSS
         align-items: center; 
         justify-content: center;
     }
+    .circular-border {
+        border-radius: 50%;
+        width: 272px;
+        height: 274px;
+        background: linear-gradient(90deg, #0054a3, #0cd840);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* 3D/Embossed effect */
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.6), 
+            -5px -5px 15px rgba(225, 225, 163, 0.6);
+    }
+    .circular-border:hover {
+        box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.8), 
+            -5px -5px 15px rgba(0, 84, 163, 0.8);
+    }
+
+    .image-click {
+        margin-top: 40px;
+    }
+
 CSS);
 ?>
-<div class="accounting-report-index-page"> <div>
-        <?= Html::a(Html::image(App::setting('image')->primary_logo, ['w' => 200]) . '
-        <p class="lead text-center font-weight-bold my-2">
-            Login to Manager.io
-        </p>', App::setting('system')->manager_io_link, [
-            'class' => 'btn btn-lg btn-outline-primary font-weight-bolder',
-            'target' => '_blank'
-        ]) ?>
-    </div>
+<div class="accounting-report-index-page">
+    <a href="<?= App::setting('system')->manager_io_link ?>" target="_blank" class="text-center">
+        <div class="circular-border">
+            <img class="image-click" src="/default/account-it-right-circle-logo.png" alt="">
+        </div>
+        <p class="lead font-weight-bold mt-3 text-uppercase">Login to Manager.io</p>
+    </a>
 </div>
